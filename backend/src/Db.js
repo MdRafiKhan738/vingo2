323016a3042d29d8908b80c3a6f4ec8bf06db31d
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2025-12-06 21:33:16 +0600

    Improve database connection handling in Db.js
    Refactor database connection logic to include error handling and connection options.

--- backend/src/Db.js
+++ backend/src/Db.js
@@ -1,19 +1,20 @@
-const { default: mongoose } = require("mongoose")
-const dotenv=require("dotenv")
-dotenv.config()
-const mongourl=process.env.MONGO_URL
-const connectdb=async()=>{
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
+const connectdb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000
+    });
 
+    console.log("DB connected successfully");
+  } catch (error) {
+    console.error("DB Error =>", error.message);
+    throw error; // THIS IS IMPORTANT FOR VERCEL
+  }
+};
 
-    try {
-        await mongoose.connect(mongourl)
-
-        console.log(`db connected successfully`)
-        
-    } catch (error) {
-        console.log(`db error :${error}`)
-        
-    }
-}
-module.exports=connectdb
+module.exports = connectdb;
