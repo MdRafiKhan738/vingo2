8bc985003b32d42f7c456440b3089114548fb246
MdRafiKhan738 <wonderfulnatuare@gmail.com>
2025-12-06 20:25:12 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,33 @@
-const express=require("express")
-const app=express()
-const cors=require("cors")
-const dotenv=require("dotenv")
-const cookieParser = require("cookie-parser")
-const connectdb = require("./src/Db")
-const userrouter=require("./src/route/AuthRoute.js")
-const shoprouter=require("./src/route/ShopRoute.js")
-const itemrouter=require("./src/route/ItemRoute.js")
-const orderouter=require("./src/route/OrderRoute.js")
-const http =require("http")
-const {Server}=require("socket.io")
-const { sockethandler } = require("./socket.js")
-dotenv.config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const connectdb = require("./src/Db");
 
-app.get("/",async()=>{
-    console.log("Welcomes to vingo backend system")
+dotenv.config();
 
+connectdb();
 
-
-
-
-
-})
-app.use(express.json())
-const server=http.createServer(app)
-const io=new Server(server,{
-  cors:({
-    origin:['http://localhost:5173','http://localhost:5174',"http://localhost:5175"],
-    methods:['POST','GET',
-        'PUT','PATCH'
-    ],
-    credentials:true
-
-
-
-
-
-
-})
-
-
-})
-
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-    origin:['http://localhost:5173','http://localhost:5174',"http://localhost:5175"],
-    methods:['POST','GET',
-        'PUT','PATCH'
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "http://localhost:5175"
     ],
-    credentials:true
+    credentials: true,
+    methods: ["GET","POST","PUT","PATCH"]
+}));
 
+app.get("/", (req, res) => {
+    res.send("Vingo backend running!");
+});
 
+app.use("/user", require("./src/route/AuthRoute"));
+app.use("/shop", require("./src/route/ShopRoute"));
+app.use("/item", require("./src/route/ItemRoute"));
+app.use("/order", require("./src/route/OrderRoute"));
 
-
-
-
-}))
-app.use("/user",userrouter)
-app.use("/shop",shoprouter)
-app.use("/item",itemrouter)
-app.use("/order",orderouter)
-const port=process.env.port
-
-server.listen(port,async()=>{
-
-  try {
-    console.log(   `server is running on ${port}`)
-    connectdb()
-    
-  } catch (error) {
-    console.log(`server error ${error}`)
-    
-  }
-})
+module.exports = app;
